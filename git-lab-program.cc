51960157f8afabfd40812a1cc37738f9f6c5daf9
tumblinx <tumblindrew@gmail.com>
2023-09-08 17:54:46 -0400

    Initial commit

---
+++ git-lab-program.cc
@@ -0,0 +1,18 @@
+/*
+*           File: git-lab-program.cc
+*           Author: Drew Tumblin
+*           Date: 09/08/2023
+*   Description: This is my C++ file for the Lab for CS 2400
+*
+*/
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << "Hello Git!!" << endl;
+}
